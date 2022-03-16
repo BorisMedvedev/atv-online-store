3414a2fa0c983a2fddede51809fe44bc335c56ec
Your Name <you@example.com>
2022-03-16 21:25:43 +0300

    banner-section

--- js/main.js
+++ js/main.js
@@ -1,3 +1,7 @@
-$(function(){
-
+$(function () {
+  $('.banner-section__slider').slick({
+    dots: true,
+    prevArrow: '<button class="banner-section__slider-btn banner-section__slider-btnprev"><img src="./images/icons/arrow-prev.svg" alt=""></button>',
+    nextArrow: '<button class="banner-section__slider-btn banner-section__slider-btnnext"><img src="./images/icons/arrow-next.svg" alt=""></button>'
+  });
 });
