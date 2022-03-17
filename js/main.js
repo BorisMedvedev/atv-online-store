5322a42d85bb7ee2a58a98f1b5a88ce83aa55b03
Your Name <you@example.com>
2022-03-17 21:52:40 +0300

    add search

--- js/main.js
+++ js/main.js
@@ -1,7 +1,17 @@
 $(function () {
-  $('.banner-section__slider').slick({
+  $(".banner-section__slider").slick({
     dots: true,
     prevArrow: '<button class="banner-section__slider-btn banner-section__slider-btnprev"><img src="./images/icons/arrow-prev.svg" alt=""></button>',
-    nextArrow: '<button class="banner-section__slider-btn banner-section__slider-btnnext"><img src="./images/icons/arrow-next.svg" alt=""></button>'
+    nextArrow: '<button class="banner-section__slider-btn banner-section__slider-btnnext"><img src="./images/icons/arrow-next.svg" alt=""></button>',
+  });
+
+  $(".search__tabs-btn").on("click", function (e) {
+    e.preventDefault();
+
+    $(".search__tabs-btn").removeClass("search__tabs-btn--active");
+    $(".search__content-item").removeClass("search__content-item--active");
+
+    $(this).addClass("search__tabs-btn--active");
+    $($(this).attr("href")).addClass("search__content-item--active");
   });
 });
