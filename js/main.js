1249af529f962a0ca4998479b20920d1fc67eace
Your Name <you@example.com>
2022-03-26 11:13:53 +0300

    9999

--- js/main.js
+++ js/main.js
@@ -3,7 +3,15 @@ $(function () {
   $(".banner-section__slider").slick({
     dots: true,
     prevArrow: '<button class="banner-section__slider-btn banner-section__slider-btnprev"><img src="./images/icons/arrow-prev.svg" alt=""></button>',
-    nextArrow: '<button class="banner-section__slider-btn banner-section__slider-btnnext"><img src="./images/icons/arrow-next.svg" alt=""></button>'
+    nextArrow: '<button class="banner-section__slider-btn banner-section__slider-btnnext"><img src="./images/icons/arrow-next.svg" alt=""></button>',
+    responsive: [
+      {
+        breakpoint: 968,
+        settings: {
+          arrows: false
+        }
+      },
+    ]
   });
 
   $(".product-slider").slick({
